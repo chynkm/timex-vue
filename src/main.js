748bf3c60f418a4d289e7b4196ec94b5901f62fe
Karthik M <chynkm@gmail.com>
2019-07-13 19:10:10 +0530

    13-07-2019

--- src/main.js
+++ src/main.js
@@ -6,11 +6,14 @@ import VueNotify from 'vue-notifyjs'
 import 'vue-notifyjs/themes/default.css'
 import PaperDashboard from "./plugins/paperDashboard";
 import "vue-notifyjs/themes/default.css";
+import VueAxios from 'vue-axios'
+import VueAuthenticate from 'vue-authenticate'
 
 window.axios = axios;
 
 Vue.use(PaperDashboard);
 Vue.use(VueNotify)
+Vue.use(VueAxios, axios)
 
 /* eslint-disable no-new */
 new Vue({
